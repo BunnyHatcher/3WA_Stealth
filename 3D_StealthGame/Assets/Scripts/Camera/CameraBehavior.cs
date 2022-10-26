b13e81c710a25954da2f2ac6b38756d0eba542a0
Martin Engelhaus <martinengelhaus@gmx.de>
2022-10-26 12:37:16 +0200

    camera is working - but player lookat behavior needs rework

--- 3D_StealthGame/Assets/Scripts/Camera/CameraBehavior.cs
+++ 3D_StealthGame/Assets/Scripts/Camera/CameraBehavior.cs
@@ -9,8 +9,13 @@ public class CameraBehavior : MonoBehaviour
     [SerializeField] private float _rotateSpeed;
 
     private Transform _target;
+    private Transform _playerTransform = null;
     private bool _rightToLeft = true;
 
+    
+    
+    
+    
     private void Start()
     {
         _target = _rightLimit;
@@ -18,29 +23,54 @@ public class CameraBehavior : MonoBehaviour
 
     private void Update()
     {
-        Vector3 lookPosition = Vector3.RotateTowards(transform.forward, _target.position, _rotateSpeed * Time.deltaTime, 0f);
+        if (_playerTransform != null)
+        {
+            transform.LookAt(_playerTransform.position);
+        }
+
+        else
+        {
+            PingPong();
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Debug.Log("Player detected");
+            _playerTransform = other.transform;
+        }
+    }
+
+
+
+    private void PingPong()
+    {
+        Vector3 lookPosition = Vector3.RotateTowards(transform.forward, _target.localPosition, _rotateSpeed * Time.deltaTime, 0f);
         Quaternion lookRotation = Quaternion.LookRotation(lookPosition);
 
         transform.rotation = lookRotation;
 
-        Quaternion targetRotation = Quaternion.LookRotation(_target.position); // rotation that we want to reach
+        Quaternion targetRotation = Quaternion.LookRotation(_target.localPosition); // rotation that we want to reach
 
-        if(Quaternion.Angle(transform.rotation, targetRotation) < 0.5f) // if we get close to reaching the same rotation as the target rotation...
+        if (Quaternion.Angle(transform.rotation, targetRotation) < 0.5f) // if we get close to reaching the same rotation as the target rotation...
         {
 
             _rightToLeft = !_rightToLeft; // ... the rotation direction is inversed
 
-            if(_rightToLeft)
+            if (_rightToLeft)
             {
-            _target = _rightLimit;
+                _target = _rightLimit;
             }
 
             else
             {
-            _target = _leftLimit;
+                _target = _leftLimit;
             }
-        }
 
+        }
     }
 
 }
